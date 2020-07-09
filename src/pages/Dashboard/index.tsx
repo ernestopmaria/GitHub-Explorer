be272f7275d3d070698e3855a9fa1f2d223684af
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2020-07-09 22:31:35 +0100

    css

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 import { Title, Form, Repositories } from './styles';
 import logoImg from '../../assets/logo.svg';
 
@@ -17,7 +18,39 @@ const Dashboard: React.FC = () => {
                 <button type="submit">Pesquisar</button>
             </Form>
             <Repositories>
-                <a href="teste" />
+                <a href="teste">
+                    <img
+                        src="https://avatars3.githubusercontent.com/u/58423237?s=460&u=f39d1d5e73424473bc991b93bb36566ecb015b76&v=4"
+                        alt="Ernesto Maria"
+                    />
+                    <div>
+                        <strong>Gostack 12</strong>
+                        <p>Aprendendo React JS de ima forma Imersiva</p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+                <a href="teste">
+                    <img
+                        src="https://avatars3.githubusercontent.com/u/58423237?s=460&u=f39d1d5e73424473bc991b93bb36566ecb015b76&v=4"
+                        alt="Ernesto Maria"
+                    />
+                    <div>
+                        <strong>Gostack 12</strong>
+                        <p>Aprendendo React JS de ima forma Imersiva</p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+                <a href="teste">
+                    <img
+                        src="https://avatars3.githubusercontent.com/u/58423237?s=460&u=f39d1d5e73424473bc991b93bb36566ecb015b76&v=4"
+                        alt="Ernesto Maria"
+                    />
+                    <div>
+                        <strong>Gostack 12</strong>
+                        <p>Aprendendo React JS de ima forma Imersiva</p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
             </Repositories>
         </>
     );
