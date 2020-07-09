2069822d1f9e26612fdc047b441d19efd6d954a7
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2020-07-09 21:52:03 +0100

    add-styled-components

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
-import { Title } from './styles';
+import { Title, Form, Repositories } from './styles';
+import logoImg from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
-    return <Title>Explore repositorios no Github</Title>;
+    return (
+        <>
+            <img src={logoImg} alt="Github Explorer" />
+            <Title>Explore repositorios no Github</Title>
+
+            <Form action="">
+                <input
+                    type="text"
+                    placeholder="Digite o nome do Repositorio"
+                    id=""
+                />
+                <button type="submit">Pesquisar</button>
+            </Form>
+            <Repositories>
+                <a href="teste" />
+            </Repositories>
+        </>
+    );
 };
 export default Dashboard;
